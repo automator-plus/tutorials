6615476da6579a897742e284a5d9f9e7322fc818
Louwrens <louwjlabuschagne@gmail.com>
2021-02-16 14:15:13 +0200

    Code snippet reaching the goal, with some work-around

---
+++ ExtendScript/questions/nextClip-equal-thisClip/main.jsx
@@ -0,0 +1,37 @@
+var videoTrackNr = 0;
+var audioTrackNr = 0;
+var overwrite = true;
+
+var project = app.project;
+var sequence = project.activeSequence;
+
+var videoTracks = sequence.videoTracks;
+var track = videoTracks[videoTrackNr];
+var clips = track.clips;
+var thisClip = clips[0];
+var nextClip = clips[1];
+var nextClipAudio = sequence.audioTracks[audioTrackNr].clips[1];
+
+var thisClipEnd = thisClip.end;
+
+
+var nextClipProjectItem = nextClip.projectItem;
+
+if (overwrite) {
+  sequence.overwriteClip(
+    nextClipProjectItem,
+    thisClipEnd,
+    videoTrackNr,
+    audioTrackNr
+  );
+} else {
+  sequence.insertClip(
+    nextClipProjectItem,
+    thisClipEnd,
+    videoTrackNr,
+    audioTrackNr
+  );
+}
+
+nextClip.remove(false, true); // inRipple, inAlignToVideo
+nextClipAudio.remove(false, true); // inRipple, inAlignToVideo
